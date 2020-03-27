d5e38bcb68e33b46f7f302d49177bd7a4dc0e8a3
rooooose <57755204+rooooose@users.noreply.github.com>
2020-03-27 08:25:57 +0100

    légères modifs

--- src/js/map_countries.js
+++ src/js/map_countries.js
@@ -1,16 +1,6 @@
 import $ from 'jquery';
 
 import Background from './background';
-//import {countUpAnimation} from './helpers/populationHelper';
-
-/*
-* Objectif : récupérer une citation aléatoire à partir d'une API et l'afficher
-*
-* Étapes :
-* 1- Créer une référence vers les éléments du DOM qu'on va utiliser
-* 2- Récupérer une citation aléatoire à partir de l'API de QuotesOnDesign (https://quotesondesign.com/api/)
-* 3- Afficher la citation
-* */
 
 
 export default class Map_countries {
